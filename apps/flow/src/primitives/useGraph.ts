f8434fc521889024193ff106e111d250ae08338c
Hieu Nguyen <macbookM1@MacBook-Pro.local>
2023-09-16 18:37:52 +0700

    remove listener on clean up

--- apps/flow/src/primitives/useGraph.ts
+++ apps/flow/src/primitives/useGraph.ts
@@ -1,27 +1,40 @@
 import { Graph } from '_@models/Graph';
+import { onCleanup } from 'solid-js';
 
 export const useGraph = () => {
   const graph = new Graph();
+  const onMouseDown = (e: MouseEvent) => {
+    const target = e.target;
+    if (!(target instanceof HTMLElement)) return;
+    if (target.classList.contains('edge-dragger-out')) {
+      graph.edges.edgeSrc = target.parentElement?.dataset.nodeId;
+    }
+  };
+  const onMouseUp = (e: MouseEvent) => {
+    const target = e.target;
+    if (!(target instanceof HTMLElement)) return;
+    if (target.classList.contains('edge-dragger-out')) {
+      graph.edges.edgeSrc = target.parentElement?.dataset.nodeId;
+    }
+  };
+  const onMouseMove = (e: MouseEvent) => {
+    const target = e.target;
+    if (!(target instanceof HTMLElement)) return;
+    if (target.classList.contains('edge-dragger-out')) {
+      graph.edges.edgeSrc = target.parentElement?.dataset.nodeId;
+    }
+  };
 
   // Drag to create Edges
-  {
-    document.addEventListener('mousedown', (e) => {
-      const target = e.target;
-      if (!(target instanceof HTMLElement)) return;
-      if (target.classList.contains('edge-dragger-out')) {
-        graph.edges.edgeSrc = target.parentElement?.dataset.nodeId;
-      }
-    });
-
-    document.addEventListener('mouseup', (e: any) => {
-      graph.edges.clearDraggingEdge();
-      graph.edges.createEdge(e.target?.dataset?.nodeId || e.target?.parentElement?.dataset?.nodeId);
-    });
-
-    document.addEventListener('mousemove', (e) => {
-      graph.edges.mousePos = { x: e.clientX, y: e.clientY };
-    });
-  }
+  document.addEventListener('mousedown', onMouseDown);
+  document.addEventListener('mouseup', onMouseUp);
+  document.addEventListener('mousemove', onMouseMove);
+  onCleanup(() => {
+    document.removeEventListener('mousedown', onMouseDown);
+    document.removeEventListener('mouseup', onMouseUp);
+    document.removeEventListener('mousemove', onMouseMove);
+  });
+  // Drag to create Edges
 
   return graph;
 };
